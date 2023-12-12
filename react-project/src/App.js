daa35041207120c33b0d417fe3dbb28f47cf1617
ShraddhaAmbekar <ambekarshraddha21@gmail.com>
2023-12-12 15:02:01 +0530

    react component

--- react-project/src/App.js
+++ react-project/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+//how to create a component
+//functional component
+//first letter  should be capital of component name
 
-function App() {
+//regular function
+
+// function App() {
+//   return (
+// <div>
+//   <h1>Hello this is my first component App</h1>
+// </div>
+//   );
+// }
+
+//arrow function
+const App=() =>{
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<div>
+  <img src="https://images.pexels.com/photos/674010/pexels-photo-674010.jpeg?auto=compress&cs=tinysrgb&w=600" alt="img"/>
+  <h1>Hello this is my first component App</h1>
+</div>
   );
 }
-
 export default App;
