e2ab6dfce2afa4429a2a5b011844b4a27b1682f1
ShraddhaAmbekar <ambekarshraddha21@gmail.com>
2024-01-03 22:06:40 +0530

    calculator start

---
+++ classcompo/src/compo/Calculator.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+class Calculator extends React.Component{
+  render(){
+    return(
+      <div>
+       <button>gfdg</button>
+      </div>
+    )
+  }
+}
+export default Calculator
